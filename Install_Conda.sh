3822f705f110d4f2fd0a55cde844abb69ce98c5b
manuel <madisi1998@gmail.com>
2019-09-18 16:53:42 +0200

    First commit; Conda Installation and jupyter config

---
+++ Install_Conda.sh
@@ -0,0 +1,9 @@
+./Miniconda3-latest-Linux-x86_64.sh
+source $HOME/.bashrc
+conda env create -f environment.yml
+mkdir $HOME/jupyter-desk
+mkdir $HOME/.jupyter
+mv jupyter_notebook_config.py $HOME/.jupyter
+mv run_jupyter $(echo $PATH | cut -d':' -f1)
+mv screen_jupyter $(echo $PATH | cut -d':' -f1)
+rm Miniconda3-latest-Linux-x86_64.sh
